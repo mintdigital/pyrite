5562fa9aeeeac4e2b0dbbc71e291f40f0170a0d8
Adam <electronicbattleweapon7@gmail.com>
2010-04-12 16:08:05 +0100

    explain fixtures better

--- lib/pyrite/helpers.rb
+++ lib/pyrite/helpers.rb
@@ -1,7 +1,7 @@
 module Pyrite
   module Helpers
 
-    # Easily get the path to a fixture file
+    # Easily get the path to a fixture file from Rails.root/test/fixtures/<filename>
     def fixture(filename)
       File.join(Rails.root,'test','fixtures', filename)
     end
