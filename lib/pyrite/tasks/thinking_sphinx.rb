fc4b9e6a5c957dab33c231c09adc60e62c505ecb
Adam <electronicbattleweapon7@gmail.com>
2010-05-13 16:58:23 +0100

    use rake pyrite:sphinx:(start | stop) to enable sphinx

--- lib/pyrite/tasks/thinking_sphinx.rb
+++ lib/pyrite/tasks/thinking_sphinx.rb
@@ -1,14 +1,15 @@
 namespace :pyrite do
   namespace :sphinx do
+    desc "Call Thinking Sphinx's rebuild task at start up"
     task :start do
       Rake::Task['ts:rebuild'].invoke
+      Rake::Task['pyrite:start'].invoke
     end
 
+    desc "Call Thinking Sphinx's stop task after test"
     task :stop do
       Rake::Task['ts:stop'].invoke
+      Rake::Task['pyrite:stop'].invoke
     end
   end
-
-  task :start => 'sphinx:start'
-  task :stop  => 'sphinx:stop'
 end
