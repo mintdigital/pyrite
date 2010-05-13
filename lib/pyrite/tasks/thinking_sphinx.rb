cfcade7a2a2422097e4d5eb0ae349d9cf2a38aa4
Adam <electronicbattleweapon7@gmail.com>
2010-05-13 13:22:02 +0100

    add thinking_sphinks start and stop

---
+++ lib/pyrite/tasks/thinking_sphinx.rb
@@ -0,0 +1,17 @@
+namespace :pyrite do
+  namespace :sphinx do
+
+    task :start do
+      Rake::Task['ts:rebuild'].invoke
+    end
+
+    task :stop do
+      Rake::Task['ts:stop'].invoke
+    end
+
+  end
+
+  task :start => 'sphinx:start'
+  task :stop  => 'sphinx:stop'
+
+end
