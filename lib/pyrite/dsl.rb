565c22d023c9197535c13bef170d55f1303c78d4
Adam <electronicbattleweapon7@gmail.com>
2010-06-16 15:08:31 +0100

    is this the correct syntax?

--- lib/pyrite/dsl.rb
+++ lib/pyrite/dsl.rb
@@ -61,9 +61,9 @@ module Pyrite
     def wait_for(element)
       case element
         when :page_load
-          browser.wait(:wait_for => :page)
+          browser.wait_for(:wait_for => :page)
         when :ajax
-          browser.wait(:wait_for => :ajax, :javascript_framework => Pyrite.js_framework)
+          browser.wait_for(:wait_for => :ajax, :javascript_framework => Pyrite.js_framework)
         else
           browser.wait_for :element => "css=#{element}"
       end
