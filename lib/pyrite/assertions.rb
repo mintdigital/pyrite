cff78a7d9bdd025b2980a5b5e47425586bd58325
Adam <electronicbattleweapon7@gmail.com>
2010-07-20 11:42:54 +0100

    forgot the bloody assertions

--- lib/pyrite/assertions.rb
+++ lib/pyrite/assertions.rb
@@ -11,15 +11,15 @@ module Pyrite
       assert !browser.text?(text), msg || "Text '#{text}' not found on page"
     end
 
-    # Assert an element appears on the page via CSS selector
+    # Assert an element appears on the page via jquery selector
     def assert_element(selector, msg=nil)
-      assert browser.element?("css=#{selector}"),
+      assert browser.element?("jquery=#{selector}"),
         msg || "Element '#{selector}' not found on page"
     end
 
-    # Assert an element does not appear on the page via CSS selector
+    # Assert an element does not appear on the page via jquery selector
     def assert_no_element(selector, msg=nil)
-      assert !browser.element?("css=#{selector}"),
+      assert !browser.element?("jquery=#{selector}"),
         msg || "Element '#{selector}' was found on page but shouldn't be"
     end
   end
